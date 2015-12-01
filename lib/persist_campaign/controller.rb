37a5d5dbe004571e2cdaf85e15e319b1ab200d93
Florian Weber <csshsh@gmail.com>
2015-12-01 15:16:50 +0700

    Rails 4.2 support

--- lib/persist_campaign/controller.rb
+++ lib/persist_campaign/controller.rb
@@ -8,10 +8,10 @@ module PersistCampaign
     module InstanceMethods
       # Solution provided by ndp's save-the-campaign gem (http://github.com/ndp/save-the-campaign),
       # reproduced here under the MIT license
-      def _compute_redirect_to_location_with_persist_campaign_params(options = {})
+      def _compute_redirect_to_location_with_persist_campaign_params(request, options = {})
         keys = PersistCampaign.config.keys.map(&:to_s)
         to_persist = request.params.slice(*keys)
-        url = _compute_redirect_to_location_without_persist_campaign_params(options)
+        url = _compute_redirect_to_location_without_persist_campaign_params(request, options)
 
         unless to_persist.empty?
           url << (url =~ /\?/ ? '&' : '?')
