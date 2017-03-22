680ad875fd0d32a259acd81b01b680423b7e48fb
James Adam <james@lazyatom.com>
2017-03-22 11:39:25 +0000

    Avoid deprecation warnings in tests with Rails 5
    This shims around the changed controller testing DSL for Rails 5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,22 @@ require 'rspec/rails'
 
 Rails.backtrace_cleaner.remove_silencers!
 
+module Rails5ControllerSpecShim
+  module InstanceMethods
+    private
+    def get(action, params = {})
+      super(action, params: params)
+    end
+  end
+
+  def self.included(base)
+    if Gem::Version.new(Rails.version) >= Gem::Version.new('5')
+      base.prepend(InstanceMethods)
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
+  config.include Rails5ControllerSpecShim, type: :controller
 end
