65eaf385f81c7f6f8dd12fc9de3b98e72ec3d00b
James Adam <james@lazyatom.com>
2017-03-22 11:33:37 +0000

    Make test app routes explicit
    Dynamic controller and action segments are deprecated in Rails 5

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get ':controller(/:action(/:id))(.:format)'
+  get 'example/foo', to: 'example#foo'
+  get 'example/action_with_params', to: 'example#action_with_params'
+  get 'example/bar', to: 'example#bar'
 end
